64d91bf4640e6cc8662ce895bff82b0c91b1deb6
rusty <blaze121304@gmail.com>
2024-10-28 11:08:32 +0900

    commit q2

---
+++ Q2_ConvertCase.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Q2_ConvertCase {
+    public static void main(String[] args) {
+        String sol;
+        Scanner in=new Scanner(System.in);
+        String str= in.next();
+        System.out.println("STR: " + str);
+
+        sol = solution(str);
+
+        System.out.println("solution : " + sol);
+
+    }
+
+    public static String solution(String input1) {
+//        String answer = "";
+//        for (int i = 0; i < input1.length(); i++) {
+//            if(Character.isLowerCase(input1.charAt(i))){
+//               answer += Character.toUpperCase(input1.charAt(i));
+//            }else {
+//                answer += Character.toLowerCase(input1.charAt(i));
+//            }
+//        }
+        StringBuilder answer = new StringBuilder();
+        for(char ch : input1.toCharArray() ){
+            if(Character.isLowerCase(ch)){
+                answer.append(Character.toUpperCase(ch));
+            }else {
+                answer.append(Character.toLowerCase(ch));
+            }
+        }
+        return answer.toString();
+    }
+}
